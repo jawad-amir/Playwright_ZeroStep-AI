a6d55db3c87e4faf0a91682be4f9be31aab6c792
Jawad Amir <jawad.amir466@gmail.com>
2024-03-24 03:48:09 +0500

    updated

--- web-e2e/specs/zeroStepForm.spec.ts
+++ web-e2e/specs/zeroStepForm.spec.ts
@@ -1,13 +1,13 @@
-import { test } from '@playwright/test'
-import { ai } from '@zerostep/playwright'
+import { test } from '@playwright/test';
+import { ai } from '@zerostep/playwright';
 
   test('Search on Google with Playwright AI', async ({ page }) => {
-    await page.goto('https://www.google.com')
-    await page.waitForURL('https://www.google.com')
+    await page.goto('https://www.google.com');
+    await page.waitForURL('https://www.google.com');
 
-    await ai('In the search field type Playwright', { page, test })
-    await ai('Wait for the results to appear in the dropdown', { page, test})
-    await ai('Click on the First Option from the dropdown', { page, test })
-    await ai('Click on the first link from the Search Results', { page,test})
-    await ai('The Playwright hyperlink should be visible on the header of the page', { page, test })
-  })
+    await ai("In the search field type Playwright", { page, test });
+    await ai("Wait for the results to appear in the dropdown", { page, test});
+    await ai("Click on the First Option from the dropdown", { page, test });
+    await ai("Click on the first link from the Search Results", { page,test});
+    await ai("The Playwright hyperlink should be visible on the header of the page", { page, test });
+  });
