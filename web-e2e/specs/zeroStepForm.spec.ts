36dbb19c9c3909f861db781f14917f4d461a0e17
Jawad Amir <jawad.amir466@gmail.com>
2024-03-24 07:34:52 +0500

    Update zeroStepForm.spec.ts

--- web-e2e/specs/zeroStepForm.spec.ts
+++ web-e2e/specs/zeroStepForm.spec.ts
@@ -4,6 +4,9 @@ import { ai } from '@zerostep/playwright'
   test('Search on Google with Playwright AI', async ({ page }) => {
 
     await ai('Navigate to this URL https://www.google.com', { page, test})
+
+    await page.waitForLoadState('networkidle');
+    
     await ai('In the search field type Playwright', { page, test })
     await ai('Wait for the results to appear in the dropdown', { page, test})
     await ai('Click on the First Option from the dropdown', { page, test })
